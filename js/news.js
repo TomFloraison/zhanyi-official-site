f64c3d39846acfd6b1ee26b2c3259645508f8177
xiaoyu.lu <xiaoyu.lu@floraison.me>
2015-01-20 10:57:15 +0800

    basic version
    index.html, zen_englightment.html, zen_enlightment_pages,
    goldenpea.html, news_pages,

---
+++ js/news.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    $("#jinrong_weixin_a").hover(function () {
+        $("#jinrong_weixin").toggle(true);
+    }, function () {
+        $("#jinrong_weixin").toggle(false);
+    });
+
+})
